1c780d5f2a975374ddb8c8241d40ca1fc07daab8
DIEGO PASSOS COSTA <080101286@prof.uninassau.edu.br>
2019-11-12 21:09:09 -0300

    InitialCommit

---
+++ Boneco/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace Boneco
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var th = new Thread(starta);
+            th.Start();
+
+        }
+
+        public static void starta()
+        {
+            Boneco b = new Boneco();
+            do
+            {
+                b.anda();
+                Console.WriteLine("Andando----");
+                Console.WriteLine(b.informacoes());
+                Thread.Sleep(1000);
+                if (b.getEnergia() <= 0)
+                {
+                    do
+                    {
+                        Console.WriteLine("descansando");
+                        b.setEnergia();
+                        Console.WriteLine(b.informacoes());
+                        Thread.Sleep(500);
+                    } while (b.getEnergia() < 10);
+                    Console.WriteLine("recuperado... voltando em 3 segundos");
+                    Thread.Sleep(3000);
+                }
+            } while (true);
+
+        }
+
+
+
+        class Boneco
+        {
+            private int distancia = 0;
+            private int energia = 10;
+
+            public Boneco()
+            {
+
+            }
+
+            public int getEnergia()
+            {
+                return this.energia;
+            }
+            public void setEnergia()
+            {
+                energia = energia + 1;
+            }
+            public void anda()
+            {
+                distancia = distancia + 1;
+                energia = energia - 1;
+            }
+
+            public void descansa()
+            {
+                energia = energia + 1;
+            }
+
+            public string informacoes()
+            {
+                return "energia: " + energia + "\ndiatancia:" + distancia + "\n";
+            }
+        }
+    }
+}
